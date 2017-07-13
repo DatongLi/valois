977b56cde9fb89400464ce202cbb2e7cae20c0c3
osdaniellee <osdaniellee@gmail.com>
2017-07-13 09:36:48 +0800

    adjust the code structure

--- mempool/src/mempool.h
+++ mempool/src/mempool.h
@@ -23,26 +23,32 @@ public:
     }
     Mempool(const Mempool&) = delete;
 
-    T* GetElem() {
-        long unum = _used_num.load(std::memory_order_acquire);
-        if(unum < _elem_num) {
-            _used_num.fetch_add(1, std::memory_order_release);
-            T* elem = _freelist.front();
-            _freelist.pop();
-            return elem;
-        } else {
-            std::cout << "mempool empty" << std::endl;
-            return nullptr;
-        }
-    }
-    bool PutElem(T* elem) {
-        _used_num.fetch_sub(1, std::memory_order_release);
-        _freelist.push(elem);
-        return true;
-    }
+    T* GetElem();
+    bool PutElem(T* elem);
 
 private:
     long _elem_num;
     std::atomic<long> _used_num;
     std::queue<T *> _freelist;
 };
+
+template<class T>
+T* Mempool<T>::GetElem() {
+    long unum = _used_num.load(std::memory_order_acquire);
+    if(unum < _elem_num) {
+        _used_num.fetch_add(1, std::memory_order_release);
+        T* elem = _freelist.front();
+        _freelist.pop();
+        return elem;
+    } else {
+        std::cout << "mempool empty" << std::endl;
+        return nullptr;
+    }
+}
+
+template<class T>
+bool Mempool<T>::PutElem(T *elem) {
+    _used_num.fetch_sub(1, std::memory_order_release);
+    _freelist.push(elem);
+    return true;
+}
