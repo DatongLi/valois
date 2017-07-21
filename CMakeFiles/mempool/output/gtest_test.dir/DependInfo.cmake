48c6b107b33181ab9087d90e1e474ae2305470c7
osdaniellee <osdaniellee@gmail.com>
2017-07-21 23:30:01 +0800

    add gtest with cmake success

---
+++ CMakeFiles/mempool/output/gtest_test.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lidatong/work/libpool/mempool/src/test.cpp" "/home/lidatong/work/libpool/CMakeFiles/mempool/output/gtest_test.dir/mempool/src/test.o"
+  "/home/lidatong/work/libpool/mempool/unittest/gtest_test.cpp" "/home/lidatong/work/libpool/CMakeFiles/mempool/output/gtest_test.dir/mempool/unittest/gtest_test.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "mempool/src"
+  "thirdparty/googletest-src/googletest/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/lidatong/work/libpool/thirdparty/googletest-build/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/home/lidatong/work/libpool/thirdparty/googletest-build/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/mempool/output/gtest_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mempool/output/gtest_test.dir/mempool/src/test.o"
+  "CMakeFiles/mempool/output/gtest_test.dir/mempool/unittest/gtest_test.o"
+  "mempool/output/gtest_test.pdb"
+  "mempool/output/gtest_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mempool/output/gtest_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
