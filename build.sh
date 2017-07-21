7be130caae9bbeee6516aec37ba67cec57f7c8f6
osdaniellee <osdaniellee@gmail.com>
2017-07-21 23:27:14 +0800

    add gtest

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 g++ -g -O0 -o mempool/output/TestMempool mempool/example/TestMempool.cpp -I. -Imempool/src -std=c++11
-g++ -g -O0 -o threadpool/output/TestThreadpool threadpool/example/TestThreadpool.cpp -I. -Ithreadpool/src -lpthread -std=c++11
+#g++ -g -O0 -o threadpool/output/TestThreadpool threadpool/example/TestThreadpool.cpp -I. -Ithreadpool/src -lpthread -std=c++11
+#g++ -g -O0 -o mempool/unittest/test mempool/unittest/test.cpp -I./mempool -I./mempool/src -Ithird-64/gtest/include -Ithird-64/gtest -Lthread-64/gtest -lpthread -pthread
