c6770789f844fb7e5e27d1c3f69b3b7b5f940c0f
osdaniellee <osdaniellee@gmail.com>
2017-07-27 23:53:18 +0800

    delete thirdparty libs

---
+++ thirdparty/README.md
@@ -0,0 +1,1 @@
+The third party lib will place this dir.
--- thirdparty/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/baidu/Desktop/workspace/valois/thirdparty/googletest-src/googlemock
-# Build directory: /Users/baidu/Desktop/workspace/valois/thirdparty/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
