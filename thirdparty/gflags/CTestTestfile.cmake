1d8a5811332b002a078832b46b29cdf129120294
osdaniellee <osdaniellee@gmail.com>
2017-07-30 20:55:34 +0800

    add gflags module as an independent library

---
+++ thirdparty/gflags/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lidatong/work/valois/thirdparty/gflags
+# Build directory: /home/lidatong/work/valois/thirdparty/gflags
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
