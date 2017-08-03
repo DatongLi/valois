9e5b750e927dbea4178158e22262b57e95c46746
osdaniellee <osdaniellee@gmail.com>
2017-08-03 22:30:40 +0800

    delete useless cmake/debug files

--- cmake-build-debug/thirdparty/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/baidu/Desktop/workspace/valois/cmake-build-debug/thirdparty/googletest-src/googletest
-# Build directory: /Users/baidu/Desktop/workspace/valois/cmake-build-debug/thirdparty/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
