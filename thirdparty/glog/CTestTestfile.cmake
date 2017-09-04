dcb96716cbce143cbbb2ad64ab0bda250daf0426
osdaniellee <osdaniellee@gmail.com>
2017-09-04 21:44:17 +0800

    add glog code

---
+++ thirdparty/glog/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /Users/baidu/Desktop/workspace/valois/thirdparty/glog
+# Build directory: /Users/baidu/Desktop/workspace/valois/thirdparty/glog
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(demangle "/Users/baidu/Desktop/workspace/valois/thirdparty/glog/output/demangle_unittest")
+add_test(logging "/Users/baidu/Desktop/workspace/valois/thirdparty/glog/output/logging_unittest")
+add_test(signalhandler "/Users/baidu/Desktop/workspace/valois/thirdparty/glog/output/signalhandler_unittest")
+add_test(stacktrace "/Users/baidu/Desktop/workspace/valois/thirdparty/glog/output/stacktrace_unittest")
+add_test(stl_logging "/Users/baidu/Desktop/workspace/valois/thirdparty/glog/output/stl_logging_unittest")
+add_test(symbolize "/Users/baidu/Desktop/workspace/valois/thirdparty/glog/output/symbolize_unittest")
